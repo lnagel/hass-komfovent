b9c6f38344748b72dfb8cdf9263d3378e403f1c3
Lenno Nagel <lenno@namespace.ee>
2025-03-04 18:03:13 +0200

    Move down

--- custom_components/komfovent/const.py
+++ custom_components/komfovent/const.py
@@ -14,84 +14,7 @@ from typing import Final
 
 DOMAIN = "komfovent"
 
-class OperationMode(IntEnum):
-    """Operation modes."""
-    STANDBY = 0
-    AWAY = 1
-    NORMAL = 2
-    INTENSIVE = 3
-    BOOST = 4
-    KITCHEN = 5
-    FIREPLACE = 6
-    OVERRIDE = 7
-    HOLIDAY = 8
-    AIR_QUALITY = 9
-    OFF = 10
 
-class SchedulerMode(IntEnum):
-    """Scheduler operation modes."""
-    STAY_AT_HOME = 0
-    WORKING_WEEK = 1
-    OFFICE = 2
-    CUSTOM = 3
-
-class AutoModeControl(IntEnum):
-    """Auto mode control types."""
-    SCHEDULING = 0
-    AIR_QUALITY = 1
-
-class TemperatureControl(IntEnum):
-    """Temperature control types."""
-    SUPPLY = 0
-    EXTRACT = 1
-    BALANCE = 2
-    ROOM = 3
-
-class FlowControl(IntEnum):
-    """Flow control types."""
-    CAV = 0
-    VAV = 1
-    DCV = 2
-
-class CoilType(IntEnum):
-    """Coil types."""
-    HOT_WATER = 0
-    COLD_WATER = 1
-    COMBI = 2
-
-class AirQualitySensorType(IntEnum):
-    """Air quality sensor types."""
-    NOT_INSTALLED = 0
-    CO2 = 1
-    VOC = 2
-    RH = 3
-
-class OverrideMode(IntEnum):
-    """Override mode types."""
-    ALL_TIME = 0
-    IF_ON = 1
-    IF_OFF = 2
-
-class HolidayMicroventilation(IntEnum):
-    """Holiday microventilation modes."""
-    ONCE_PER_DAY = 1
-    TWICE_PER_DAY = 2
-    THRICE_PER_DAY = 3
-    FOUR_TIMES_PER_DAY = 4
-
-class ResetSettings(IntEnum):
-    """Reset settings options."""
-    AWAY = 1
-    NORMAL = 2
-    INTENSIVE = 3
-    BOOST = 4
-    HOLIDAYS = 5
-    OVERRIDE = 6
-    KITCHEN = 7
-    FIREPLACE = 8
-    AIR_QUALITY = 9
-    ECO = 10
-    ADVANCED = 11
 DEFAULT_NAME = "Komfovent"
 DEFAULT_HOST: Final = "10.20.2.60"
 DEFAULT_PORT = 502
@@ -344,3 +267,82 @@ SCHEDULER_MODES = {
     2: "Office",
     3: "Custom"
 }
+
+class OperationMode(IntEnum):
+    """Operation modes."""
+    STANDBY = 0
+    AWAY = 1
+    NORMAL = 2
+    INTENSIVE = 3
+    BOOST = 4
+    KITCHEN = 5
+    FIREPLACE = 6
+    OVERRIDE = 7
+    HOLIDAY = 8
+    AIR_QUALITY = 9
+    OFF = 10
+
+class SchedulerMode(IntEnum):
+    """Scheduler operation modes."""
+    STAY_AT_HOME = 0
+    WORKING_WEEK = 1
+    OFFICE = 2
+    CUSTOM = 3
+
+class AutoModeControl(IntEnum):
+    """Auto mode control types."""
+    SCHEDULING = 0
+    AIR_QUALITY = 1
+
+class TemperatureControl(IntEnum):
+    """Temperature control types."""
+    SUPPLY = 0
+    EXTRACT = 1
+    BALANCE = 2
+    ROOM = 3
+
+class FlowControl(IntEnum):
+    """Flow control types."""
+    CAV = 0
+    VAV = 1
+    DCV = 2
+
+class CoilType(IntEnum):
+    """Coil types."""
+    HOT_WATER = 0
+    COLD_WATER = 1
+    COMBI = 2
+
+class AirQualitySensorType(IntEnum):
+    """Air quality sensor types."""
+    NOT_INSTALLED = 0
+    CO2 = 1
+    VOC = 2
+    RH = 3
+
+class OverrideMode(IntEnum):
+    """Override mode types."""
+    ALL_TIME = 0
+    IF_ON = 1
+    IF_OFF = 2
+
+class HolidayMicroventilation(IntEnum):
+    """Holiday microventilation modes."""
+    ONCE_PER_DAY = 1
+    TWICE_PER_DAY = 2
+    THRICE_PER_DAY = 3
+    FOUR_TIMES_PER_DAY = 4
+
+class ResetSettings(IntEnum):
+    """Reset settings options."""
+    AWAY = 1
+    NORMAL = 2
+    INTENSIVE = 3
+    BOOST = 4
+    HOLIDAYS = 5
+    OVERRIDE = 6
+    KITCHEN = 7
+    FIREPLACE = 8
+    AIR_QUALITY = 9
+    ECO = 10
+    ADVANCED = 11
