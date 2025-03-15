446caea06d2e775f5f8f67aefbd3082a00b2f79d
Lenno Nagel <lenno@namespace.ee>
2025-03-15 22:53:48 +0200

    Add version number logic comment

--- custom_components/komfovent/sensor.py
+++ custom_components/komfovent/sensor.py
@@ -641,6 +641,12 @@ class FirmwareVersionSensor(KomfoventSensor):
         if value is None:
             return None
 
+        # 1st number 8bit <<24
+        # 2nd number 4bit <<20
+        # 3rd number 8bit <<12
+        # 4th number 12bit <<0
+        # Example: 18886660 => 1.2.3.4
+
         v1 = (value >> 24) & 0xFF
         v2 = (value >> 20) & 0xF
         v3 = (value >> 12) & 0xFF
