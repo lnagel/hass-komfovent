9f55a1f8106669baf0c75b2f600bca386f6db4a6
Lenno Nagel <lenno@namespace.ee>
2025-03-16 11:02:35 +0200

    refactor: Improve docstring formatting for get_version_from_int function

--- custom_components/komfovent/helpers.py
+++ custom_components/komfovent/helpers.py
@@ -4,13 +4,15 @@ from __future__ import annotations
 
 
 def get_version_from_int(value: int) -> tuple[int, int, int, int]:
-    """Convert integer version to component numbers.
+    """
+    Convert integer version to component numbers.
 
     Args:
         value: Integer containing version information packed as bitfields
 
     Returns:
         Tuple of (major, minor, patch, build) version numbers
+
     """
     # 1st number 8bit <<24
     # 2nd number 4bit <<20
