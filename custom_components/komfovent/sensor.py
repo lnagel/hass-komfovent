935ccc68880278a56a126e19b29d40618316faac
Lenno Nagel <lenno@namespace.ee>
2025-03-26 11:39:56 +0200

    style: Fix whitespace in sensor.py

--- custom_components/komfovent/sensor.py
+++ custom_components/komfovent/sensor.py
@@ -236,7 +236,7 @@ async def create_sensors(coordinator: KomfoventCoordinator) -> list[KomfoventSen
                 register_id=registers.REG_WATER_COOLER,
                 entity_description=SensorEntityDescription(
                     key="water_cooler",
-                    name="Water Cooler", 
+                    name="Water Cooler",
                     native_unit_of_measurement=PERCENTAGE,
                     state_class=SensorStateClass.MEASUREMENT,
                     suggested_display_precision=0,
